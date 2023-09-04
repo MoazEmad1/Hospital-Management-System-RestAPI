1b2b0325c0a3b60244861b2a815305af7a233f1a
Moaz Emad Ahmed Awad Hussein <103341306+MoazEmad1@users.noreply.github.com>
2023-09-04 13:28:14 +0300

    unique attributes edit

--- src/main/java/com/example/hospital/entity/User.java
+++ src/main/java/com/example/hospital/entity/User.java
@@ -14,7 +14,9 @@ public abstract class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String username;
+    @Column(unique = true)
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
